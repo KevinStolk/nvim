a5f6198f90a547b8e6d18805045249d980e65be4
KevinStolk <stolkkevin02@gmail.com>
2024-05-22 09:46:23 +0200

    Added Overseer autocmd watcher for Go

--- after/plugin/overseer.lua
+++ after/plugin/overseer.lua
@@ -3,4 +3,27 @@ if not status then
 	return
 end
 
-overseer.setup()
+overseer.setup({
+	templates = { "builtin", "user.run_script" },
+})
+
+vim.api.nvim_create_user_command("WatchRun", function()
+	local overseer_cmd = require("overseer")
+	overseer_cmd.run_template({ name = "run script" }, function(task)
+		if task then
+			task:add_component({ "restart_on_save", paths = { vim.fn.expand("%:p") } })
+			local main_win = vim.api.nvim_get_current_win()
+			overseer.run_action(task, "open vsplit")
+			vim.api.nvim_set_current_win(main_win)
+		else
+			vim.notify("WatchRun not supported for filetype " .. vim.bo.filetype, vim.log.levels.ERROR)
+		end
+	end)
+end, {})
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+	pattern = { "*.go" },
+	callback = function()
+		vim.cmd("WatchRun")
+	end,
+})
--- lua/kevin/packer.lua
+++ lua/kevin/packer.lua
@@ -3,7 +3,7 @@ vim.cmd([[packadd packer.nvim]])
 return require("packer").startup(function(use)
 	-- Packer can manage itself
 	use("wbthomason/packer.nvim")
-
+	use("nvim-lua/plenary.nvim")
 	use({
 		"nvim-telescope/telescope.nvim",
 		requires = { { "nvim-lua/plenary.nvim" } },
